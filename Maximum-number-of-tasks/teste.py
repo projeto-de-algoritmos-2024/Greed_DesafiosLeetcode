94b29a0d6a1a86ba9fe5f2eb2c8899e98fb63078
Julio Dourado <juliodourado419@gmail.com>
2025-01-20 09:34:50 -0300

    atualizacao da solucao

--- Maximum-number-of-tasks/teste.py
+++ Maximum-number-of-tasks/teste.py
@@ -1,25 +1,34 @@
 class Solution(object):
     def maxTaskAssign(self, tarefas, trabalhadores, pilulas, forca_extra):
+
         tarefas.sort()
         trabalhadores.sort()
         
-        indice_tarefa = 0
-        indice_trabalhador = 0
-        pilulas_usadas = 0
-        tarefas_atribuidas = 0
-        
-        while indice_tarefa < len(tarefas) and indice_trabalhador < len(trabalhadores):
-            if trabalhadores[indice_trabalhador] >= tarefas[indice_tarefa]:
-                tarefas_atribuidas += 1
-                indice_tarefa += 1
-                indice_trabalhador += 1
-            else:
-                if pilulas_usadas < pilulas and trabalhadores[indice_trabalhador] + forca_extra >= tarefas[indice_tarefa]:
-                    pilulas_usadas += 1
-                    tarefas_atribuidas += 1
-                    indice_tarefa += 1
-                    indice_trabalhador += 1
+        def pode_atribuir(k):
+            indice_tarefa = k - 1
+            indice_trabalhador = len(trabalhadores) - 1
+            pilulas_restantes = pilulas
+            
+            while indice_tarefa >= 0 and indice_trabalhador >= 0:
+                if trabalhadores[indice_trabalhador] >= tarefas[indice_tarefa]:
+                    indice_tarefa -= 1
+                    indice_trabalhador -= 1
                 else:
-                    indice_trabalhador += 1
-                        
-        return tarefas_atribuidas
+                    if pilulas_restantes > 0 and trabalhadores[indice_trabalhador] + forca_extra >= tarefas[indice_tarefa]:
+                        pilulas_restantes -= 1
+                        indice_tarefa -= 1
+                        indice_trabalhador -= 1
+                    else:
+                        indice_trabalhador -= 1
+            
+            return indice_tarefa < 0
+        
+        baixo, alto = 0, len(tarefas)
+        while baixo < alto:
+            meio = (baixo + alto + 1) // 2
+            if pode_atribuir(meio):
+                baixo = meio
+            else:
+                alto = meio - 1
+        
+        return baixo
